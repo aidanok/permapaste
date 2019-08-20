f14a882592f20caaa8b78c88f8eaf563d09061e8
aokisok <sevenclickok@gmail.com>
2019-08-20 14:06:50 +0100

    move some imports to main.js

--- src/app-model/markdown.ts
+++ src/app-model/markdown.ts
@@ -1,7 +1,5 @@
 import marked from 'marked'
 import hljs from 'highlight.js';
-import 'highlight.js/styles/github.css';
-import 'github-markdown-css/github-markdown.css'
 
 marked.setOptions({
   renderer: new marked.Renderer(),
