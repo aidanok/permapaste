2bda30b9c152d0c4aa42658464318a00b03e6b56
aokisok <sevenclickok@gmail.com>
2019-08-26 20:16:19 +0100

    reset paste editing to makdown as default

--- src/app-state/paste-editing.ts
+++ src/app-state/paste-editing.ts
@@ -48,7 +48,7 @@ export class PasteEditing {
     this.paste = {
       pasteTitle: '',
       pasteText: defaultMarkdown,
-      pasteFormat: 'plaintext',
+      pasteFormat: 'markdown',
       pastePrivacy: 'secretpass'
     }
 
